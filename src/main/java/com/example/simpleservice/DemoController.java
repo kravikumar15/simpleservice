2e17fc91840dcc9b97b28c57f206dc48ad671751
Ravi Konda <k.ravikumar15@gmail.com>
2024-10-23 07:06:20 +0530

    add new html for ui

--- src/main/java/com/example/simpleservice/DemoController.java
+++ src/main/java/com/example/simpleservice/DemoController.java
@@ -1,28 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.example.simpleservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.ResponseBody;
 /**
  *
  * @author fuser
  */
-// Annotation 
+ 
 @Controller
-// Main class 
 public class DemoController { 
+
+    @Value("${spring.application.name}")
+    String appName;
   
     @RequestMapping("/hello") 
     @ResponseBody
-  
-    // Method 
-    public String hello() 
+     public String hello() 
     { 
         return "Hello World"; 
     } 
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
 }
